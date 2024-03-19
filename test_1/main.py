d5cc7131e74e253312db9359b630e85bbcfc4f50
Tharushiwijesinghe <tharushiwijesinghe99@gmail.com>
2024-03-19 16:20:42 +0530

    water usage graph for week

--- test_1/main.py
+++ test_1/main.py
@@ -2,12 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("quality.csv")
-data = data.set_index("Date")
+sns.set_style("whitegrid", {'axes.facecolor': '#1A1F35'})
+
+data = pd.read_excel("water-usage.xlsx")
+data = data.set_index("Days")
 print(data)
 
-plt.plot(data.index,data['ph'])
+plt.bar(data.index, data['Water'],color='#0E3653', width=0.95)
+plt.xlabel("Days")
+plt.ylabel("Water(L)")
+plt.title("VISUALIZING FUTURE WATER USAGE TREND FOR COMING UP DAYS")
+plt.xticks(rotation=45)  
 plt.show()
+
+
 
 
 
